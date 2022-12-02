4aede32a2296b67bee723cbb7903fba45df1bc65
Osama Abdulghani <osama2231@hotmail.com>
2022-12-02 11:31:46 +0400

    Edit interface.py

--- interface.py
+++ interface.py
@@ -1,24 +1,96 @@
-from guizero import App, Text,PushButton, Picture, Drawing
+from tkinter import Tk, Label, Canvas, Button
+import time as t
+
+SIZE = 16
+COLOR = "white"
+
 
 class Interface:
-    app = None
-    def __init__(self,rpi,exit_app,classify,title):
-        self.rpi = rpi
-        self.app = App(title=title)
-        self.app.set_full_screen(keybind='<Escape>')
-        message = Text(self.app, text="Welcome to our Points-Based Trash Classifier System Utilizing Deep Learning Techniques.")
-        prompt = Text(self.app, text="Please place your trash in front of the camera and click the classify button, you will see the correct category on the screen and the LEDs.")
-        prompt2 = Text(self.app,text="Then kindly take your trash and throw it in the correct bin.")
-        classify = PushButton(self.app,command=classify,text="Classify")
-        classify.bg = "green"
 
-        close = PushButton(self.app,command=exit_app,text="Esc",align="bottom")
-        close.bg = 'red'
-        self.img_preview = Drawing(self.app)
-        self.img_preview.rectangle(10,10,300,300)
-        #self.img_preview.rectangle(
-    def show_image(self,img_path):
-        self.img_preview.image(0,0,img_path,width=480,height=320)
-        self.app.display()
-    def show(self):
-        self.app.display()
+    def __init__(self, rpi, exit_screen, classifying):
+        self.rpi = rpi
+        self.classify_obj = classifying
+        self.stream = Tk()
+        self.stream.attributes('-fullscreen', True)
+        self.stream.title("Trash Classifier System")
+        self.stream.minsize(width=300, height=400)
+        self.display_first_layout()
+        self.stream.mainloop()
+
+    # this function for displaying the loading page after the capture button has been clicked
+    def display_loading_layout(self):
+        self.clear_frame()
+        loading = Label(text="Capturing...", font=(COLOR, SIZE))
+        loading.place(x=200, y=160)
+
+        for i in range(16):
+            Label(self.stream, bg="#525252", width=2, height=1).place(x=(i + 12) * 12, y=200)
+
+        self.stream.update()
+        self.play_animation()
+
+    # this function will play the animation of the bar in the loading layout
+    def play_animation(self):
+        for i in range(3):
+            for j in range(16):
+                # make block red:
+                Label(self.stream, bg="#FF2E2E", width=2, height=1).place(x=(j + 12) * 12, y=200)
+                t.sleep(0.06)
+                self.stream.update_idletasks()
+                # make block gray:
+                Label(self.stream, bg="#525252", width=2, height=1).place(x=(j + 12) * 12, y=200)
+        else:
+            self.display_classification_results("Osama", "0.0001")
+
+    # this function is for deleting all the widgets inside the window(which is the stream object in our case)
+    # before moving to the next layout
+    def clear_frame(self):
+        for widgets in self.stream.winfo_children():
+            widgets.destroy()
+
+    # this function will display the classification results after the item has been classified using the model
+    # it will display the name of the category and the percentage
+    def display_classification_results(self, category_name, percentage_of_category):
+        self.clear_frame()
+        category = Label(text=f"Item category is {category_name}", font=(COLOR, SIZE))
+        category.place(x=150, y=150)
+        percentage = Label(text=f"Percentage = {percentage_of_category}%", font=(COLOR, SIZE))
+        percentage.place(x=150, y=180)
+        retaking_button = Button(text="Retake Photo", command=self.display_first_layout)
+        retaking_button.place(x=150, y=210)
+        exit_button = Button(text="Exit", command=self.stream.destroy)
+        exit_button.place(x=270, y=210)
+
+    # this function will call the classify function in the rpi class, which will stop the camera from streaming
+    # after the 'Capture' button has been clicked on. Also, it will call the display loading layout function
+    # to display the loading layout
+    def call_classify_and_loading(self):
+        self.rpi.classify()
+        self.display_loading_layout()
+
+    # this function is for displaying the first layout when you run the code.
+    # It will display some text at the top, display the camera streaming in its required position,
+    # and two buttons at the bottom one for capturing the photo and the other for stopping the program
+    def display_first_layout(self):
+        self.clear_frame()
+        welcome = Label(text="Welcome to our Trash Classifier System",
+                        font=(COLOR, SIZE))
+        welcome.pack()
+        prompt = Label(text="Click on the Capture button to classify the trash", font=(COLOR, SIZE))
+        prompt.pack()
+        camera_stream = Canvas(self.stream, width=500, height=350)
+        camera_stream.pack()
+        camera_stream.create_rectangle(0, 20, 600, 600, fill="black")
+        # this function is for playing a real-live stream
+        self.rpi.make_stream()
+        capture_button = Button(text="Capture", command=self.call_classify_and_loading)
+        capture_button.pack(side="left", padx=130)
+        exit_button = Button(text="Exit", command=self.stream.destroy)
+        exit_button.pack(side="left")
+
+
+# def classify():
+#     pass
+#
+#
+# interface = Interface(None, quit, classify)
