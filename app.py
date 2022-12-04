67ea755cb4a348d955f19aa55a4534a62a832795
Diaa-I <117300163+Diaa-I@users.noreply.github.com>
2022-12-04 22:18:26 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,36 @@
 from interface import Interface
 from camera import Camera
 from model import Model
+import time as t
 class RaspberryPIManagement:
     def __init__(self):
         ## initialize objects
         self.camera = Camera(self)
-        self.model = Model(self)
-        self.interface = Interface(self, self.exit_app,self.trigger_camera,"Trash Classifier")
-        self.interface.show()
-
+        self.model = Model(self,model_filepath=r'./model/tf_lite_model.tflite')
+        self.interface = Interface(self, self.exit_app,self.classify)
     ## define auxiliary functions
     def exit_app(self):
         quit()
 
-    def trigger_camera(self):
-        self.camera.stop_preview()
-        self.camera.myfile.close()
-        self.camera.take_photo()
+
         
     def receive_photo(self,img_path):
         self.interface.show_image(img_path)
-    
+
+    def classify(self):
+        self.stop_stream()
+        self.camera.take_photo()
+
+    def stop_stream(self):
+        self.camera.stop_stream()
+
     def trigger_model(self,img_path):
         self.model.predict_img_file(img_path)
+
     def receive_classification_data(self,dict):
         self.interface.display_classification_results(dict)
-        
+
 
 
 rpi_management = RaspberryPIManagement()
-
     
