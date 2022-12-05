ce01906956a8454c861624bb9be4bb3555631b96
Diaa-I <117300163+Diaa-I@users.noreply.github.com>
2022-12-05 23:24:22 +0400

    Update camera.py

--- camera.py
+++ camera.py
@@ -10,7 +10,6 @@ class Camera:
         self.temp_photo_path = 'temp.jpg'
         self.rpi = rpi
         self.my_file = ""
-        
     def take_photo(self):
         self.camera.capture(self.temp_photo_path)
         self.rpi.receive_photo(self.temp_photo_path)
@@ -18,7 +17,9 @@ class Camera:
 
     def stream(self):
         self.my_file = open("my_file.jpg","wb")
-        self.camera.start_preview(fullscreen=False,window=(0,0,300,400))
+        self.camera.start_preview(fullscreen=False,window=(-80,80,680,340))
+
+        # width = self.stream.winfo_width() * 0.8, height = self.stream.winfo_height() * 0.75
 
     def stop_stream(self):
         self.camera.stop_preview()
