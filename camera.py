fd2cf2420ae028ce984bed711d32e48bf13556b1
Diaa-I <117300163+Diaa-I@users.noreply.github.com>
2022-12-05 21:10:15 +0400

    Update camera.py

--- camera.py
+++ camera.py
@@ -2,16 +2,16 @@ from picamera import PiCamera
 from time import sleep
 
 class Camera:
-    temp_photo_path = 'temp.png'
+    temp_photo_path = 'temp.jpg'
     camera = None
     def __init__(self,rpi):
         self.camera = PiCamera()
         self.camera.rotation=180
-        self.temp_photo_path = 'temp.png'
+        self.temp_photo_path = 'temp.jpg'
         self.rpi = rpi
         self.my_file = ""
+        
     def take_photo(self):
-        sleep(2)
         self.camera.capture(self.temp_photo_path)
         self.rpi.receive_photo(self.temp_photo_path)
         print("Done.")
